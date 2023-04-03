11615620a4c77f13d4d39abf77812ca1d81f2224
Ehsan Faraji Jalal <ehsan@internetnz.net.nz>
2023-04-03 12:31:03 +1200

    EPP Communication Module (#3)
    * epp module
    
    * linting
    
    * unit tests
    
    * more unit tests
    
    * remove vulnerable code
    
    * remove unused import

---
+++ pyepp/epp.py
@@ -0,0 +1,249 @@
+"""
+EPP Communicator Module
+"""
+import ssl
+import socket
+import struct
+import logging
+
+from bs4 import BeautifulSoup
+
+from pyepp.command_templates import LOGOUT_XML, LOGIN_XML, HELLO_XML
+
+LENGTH_FIELD_SIZE = 4
+CRLF_SIZE = 2
+
+
+class EppCommunicatorException(Exception):
+    """
+    EPP communicator exception
+    """
+
+
+def get_format_32():
+    """
+    Get the size of C integers. We need 32 bits unsigned.
+
+    From http://www.bortzmeyer.org/4934.html
+    """
+    format_32 = ">I"
+    if struct.calcsize(format_32) < LENGTH_FIELD_SIZE:
+        format_32 = ">L"
+        if struct.calcsize(format_32) != LENGTH_FIELD_SIZE:
+            logging.error("Integer size does not match the length size!")
+            raise EppCommunicatorException("Integer size does not match the length size!")
+    elif struct.calcsize(format_32) > LENGTH_FIELD_SIZE:
+        format_32 = ">H"
+        if struct.calcsize(format_32) != LENGTH_FIELD_SIZE:
+            logging.error("Integer size does not match the length size!")
+            raise EppCommunicatorException("Integer size does not match the length size!")
+
+    return format_32
+
+
+class EppCommunicator:
+    """
+    An EPP client for connecting to EPP server.
+    """
+
+    # pylint: disable=too-many-instance-attributes
+    def __init__(self, host, port, client_cert, client_key):
+        self._host = host
+        self._port = port
+        self._user = None
+        self._client_cert = client_cert
+        self._client_key = client_key
+
+        self._format_32 = get_format_32()
+
+        self._context = None
+        self._socket = None
+        self._ssl_socket = None
+        self.greeting = None
+
+    def _unpack_data(self, data):
+        """
+        Unpack data.
+
+        :param bytes data: data
+
+        :return: unpacked data
+        :rtype: str
+        """
+        return struct.unpack(self._format_32, data)[0]
+
+    def _pack_data(self, data):
+        """
+        Pack the data.
+
+        :param data: data
+
+        :return: bytes
+        """
+        return struct.pack(self._format_32, data)
+
+    def _read(self):
+        """
+        Read the response from the socket.
+
+        :return: Response
+        :rtype: bytes
+        """
+        length = self._ssl_socket.read(LENGTH_FIELD_SIZE)
+        buffer = bytes()
+
+        if not length:
+            return None
+
+        total_bytes = self._unpack_data(length) - LENGTH_FIELD_SIZE
+        while len(buffer) < total_bytes:
+            total_bytes = total_bytes - len(buffer)
+            buffer += self._ssl_socket.recv(total_bytes)
+            logging.info('Received %s/%s bytes', len(buffer), total_bytes)
+        return buffer
+
+    def _write(self, xml):
+        """
+        Write the request into the socket.
+
+        :param str xml: XML Command
+
+        :return: Number of send bytes
+        :rtype: int
+        """
+        # +4 for the length field itself (section 4 mandates that)
+        # +2 for the CRLF at the end
+        length = self._pack_data(len(xml) + LENGTH_FIELD_SIZE + CRLF_SIZE)
+
+        self._ssl_socket.send(length)
+        xml += "\r\n"
+        return self._ssl_socket.send(xml.encode("utf-8"))
+
+    def _execute_command(self, cmd):
+        """
+        Execute the command. Sending the request to the server and receive the response.
+
+        :param str cmd: XML command
+
+        :return: Response
+        :rtype: bytes
+        """
+        logging.debug("Sending xml to server :\n%s", cmd)
+
+        self._write(cmd)
+
+        response = self._read()
+        if response is None:
+            raise EppCommunicatorException("Cannot connect to server. Please re-login!")
+
+        logging.debug("Received xml response from server :\n%s", response)
+
+        return response
+
+    def connect(self):
+        """
+        Initial connect to the server.
+
+        :return: Greeting message
+        :rtype: xml in str
+        """
+        try:
+            self._context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            self._context.load_default_certs()
+            self._context.load_cert_chain(certfile=self._client_cert, keyfile=self._client_key)
+
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+            self._socket.settimeout(10)
+
+            self._ssl_socket = self._context.wrap_socket(self._socket, server_hostname=self._host)
+            self._ssl_socket.connect((self._host, int(self._port)))
+            self.greeting = self._read()
+            logging.debug(BeautifulSoup(self.greeting, 'xml'))
+            return self.greeting
+        except Exception as ex:
+            logging.error("Could not setup a sec sure connection. %s", str(ex))
+            raise EppCommunicatorException("Could not setup a sec sure connection") from ex
+
+    def execute(self, cmd):
+        """
+        Execute the command. Sending the request to the server and receive the response.
+
+        :param str cmd: XML Command
+
+        :return: XML Response
+        :rtype: dict
+        """
+        try:
+            if not self.greeting:
+                raise EppCommunicatorException("The connection to the server has not been established yet!")
+
+            raw_response = self._execute_command(cmd)
+            xml_response = BeautifulSoup(raw_response, 'xml')
+
+            response = xml_response.find('response')
+            result = xml_response.find('result')
+            message = result.find('msg').string
+
+            try:
+                code = int(result.get('code'))
+            except AttributeError as exc:
+                raise EppCommunicatorException("Could not get result code.") from exc
+
+            reason = None
+            if code not in (1000, 1500):
+                reason = result.find('reason').string if result.find('reason') else None
+
+            logging.debug("Command executed:\n%s", xml_response)
+
+            return {'code': code, 'message': message, 'reason': reason, 'response': str(response)}
+        except EppCommunicatorException as epp_ex:
+            raise epp_ex
+        except Exception as ex:
+            raise EppCommunicatorException(ex) from ex
+
+    def hello(self):
+        """
+        Send Hello command the server.
+
+        :return: Greeting response
+        :rtype: xml
+        """
+        logging.debug("Send Hello command to the server!")
+        greeting = self._execute_command(HELLO_XML)
+        return greeting
+
+    def login(self, user, password):
+        """
+        Login the user to EPP server.
+
+        :param str user: username
+        :param str password: password
+
+        :return: login
+        :rtype: xml
+        """
+        self._user = user
+
+        cmd = LOGIN_XML.format(user=user, password=password)
+        result = self.execute(cmd)
+
+        if result.get('code') == 1000:
+            logging.info("User %s logged in to %s:%s", self._user, self._host, self._port)
+        elif result.get('code') == 2004:
+            raise EppCommunicatorException("Incorrect user name or password. Please try again!")
+        else:
+            raise EppCommunicatorException(f"Something went wrong! Code: {result.get('code')} - Message: "
+                                           f"{result.get('message')} - Reason {result.get('reason')}")
+
+        return result
+
+    def logout(self):
+        """
+        Logout the user from EPP server.
+        """
+
+        logout = self.execute(LOGOUT_XML)
+        self._socket.close()
+        logging.info("User %s logged out from %s:%s", self._user, self._host, self._port)
+
+        return logout
--- tests/test_pyepp.py
+++ tests/test_pyepp.py
@@ -3,8 +3,175 @@ pyepp module unit tests
 """
 import unittest
 
+from unittest.mock import MagicMock, patch, Mock
+
+from pyepp.command_templates import HELLO_XML
+from pyepp.epp import EppCommunicator, EppCommunicatorException
+
 
 class PyEPPTests(unittest.TestCase):
 
-    def test_dummy(self):
-        self.assertEqual(1, 1)
+    def setUp(self):
+        self.epp_config = {
+            "host": "0.0.0.0",
+            "port": "700",
+            "client_cert": "path_to_cert",
+            "client_key": "path_to_key",
+        }
+        patch("pyepp.epp.socket.socket").start()
+        patch("pyepp.epp.ssl.SSLContext").start()
+
+    @patch.object(EppCommunicator, "_read")
+    def test_connect(self, mock_read):
+        mock_read.return_value = "greeting"
+        epp = EppCommunicator(**self.epp_config)
+        greeting = epp.connect()
+        self.assertEqual(greeting, "greeting")
+
+    @patch.object(EppCommunicator, "_read")
+    def test_connect_exception(self, mock_read):
+        mock_read.side_effect = Exception
+        epp = EppCommunicator(**self.epp_config)
+
+        self.assertRaises(EppCommunicatorException, epp.connect)
+
+    @patch.object(EppCommunicator, "_read")
+    def test_hello(self, mock_read):
+        mock_read.return_value = "greeting"
+        epp = EppCommunicator(**self.epp_config)
+        epp.connect()
+        hello = epp.hello()
+        self.assertEqual(hello, "greeting")
+
+    def test_login_1000(self):
+        epp = EppCommunicator(**self.epp_config)
+        expected_result = \
+            {'code': 1000, 'message': "Command completed successfully", 'reason': None, 'response': "response"}
+        epp.execute = MagicMock(return_value=expected_result)
+        epp.connect = MagicMock(return_value=None)
+
+        result = epp.login("user", "pass")
+        self.assertDictEqual(expected_result, result)
+
+    def test_login_2004(self):
+        epp = EppCommunicator(**self.epp_config)
+        expected_result = \
+            {'code': 2004, 'message': "Command completed successfully", 'reason': None, 'response': "response"}
+        epp.execute = MagicMock(return_value=expected_result)
+        epp.connect = MagicMock(return_value=None)
+
+        self.assertRaises(EppCommunicatorException, epp.login, "user", "pass")
+
+    def test_login_other(self):
+        epp = EppCommunicator(**self.epp_config)
+        expected_result = \
+            {'code': 3000, 'message': "Command completed successfully", 'reason': None, 'response': "response"}
+        epp.execute = MagicMock(return_value=expected_result)
+        epp.connect = MagicMock(return_value=None)
+
+        self.assertRaises(EppCommunicatorException, epp.login, "user", "pass")
+
+    def test_logout(self):
+        epp = EppCommunicator(**self.epp_config)
+        expected_result = \
+            {'code': 1500, 'message': 'Command completed successfully; ending session', 'reason': None, 'response': None}
+        epp.execute = MagicMock(return_value=expected_result)
+        epp.connect = MagicMock(return_value=None)
+        epp._socket = MagicMock(close=MagicMock())
+
+        result = epp.logout()
+        self.assertDictEqual(expected_result, result)
+
+    def test_execute_not_connected(self):
+        epp = EppCommunicator(**self.epp_config)
+        self.assertRaises(EppCommunicatorException, epp.execute, HELLO_XML)
+
+    def test_execute_no_result(self):
+        epp = EppCommunicator(**self.epp_config)
+        epp.connect = MagicMock(return_value=None)
+        raw_response = b'<?xml version="1.0" encoding="UTF-8"?>\n<epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:host="urn:ietf:params:xml:ns:host-1.0" xmlns:contact="urn:ietf:params:xml:ns:contact-1.0" xmlns:domain="urn:ietf:params:xml:ns:domain-1.0" xmlns:rgp="urn:ietf:params:xml:ns:rgp-1.0" xmlns:secDNS="urn:ietf:params:xml:ns:secDNS-1.1">\n    <response>\n            <msg>Command completed successfully</msg>\n        </result>\n        <trID>\n            <svTRID>CIRA-000057351729-0000000001</svTRID>\n        </trID>\n    </response>\n</epp>'
+        epp._execute_command = MagicMock(return_value=raw_response)
+        self.assertRaises(EppCommunicatorException, epp.execute, HELLO_XML)
+
+    def test_execute_with_error(self):
+        epp = EppCommunicator(**self.epp_config)
+        epp.connect = MagicMock(return_value=None)
+        epp.greeting = "greeting"
+        raw_response = b'<?xml version="1.0" encoding="UTF-8"?>\n<epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:host="urn:ietf:params:xml:ns:host-1.0" xmlns:contact="urn:ietf:params:xml:ns:contact-1.0" xmlns:domain="urn:ietf:params:xml:ns:domain-1.0" xmlns:rgp="urn:ietf:params:xml:ns:rgp-1.0" xmlns:secDNS="urn:ietf:params:xml:ns:secDNS-1.1">\n    <response>\n        <result code="2000">\n            <msg>There is an error</msg>\n  <reason>Because!</reseaon>\n        </result>\n        <trID>\n            <svTRID>CIRA-000057351729-0000000001</svTRID>\n        </trID>\n    </response>\n</epp>'
+        epp._execute_command = MagicMock(return_value=raw_response)
+        expected_result = \
+            {'code': 2000, 'message': 'There is an error', 'reason': 'Because!', 'response': '''<response>
+<result code="2000">
+<msg>There is an error</msg>
+<reason>Because!</reason>
+</result>
+<trID>
+<svTRID>CIRA-000057351729-0000000001</svTRID>
+</trID>
+</response>'''}
+
+        epp.connect()
+        result = epp.execute(HELLO_XML)
+        self.assertDictEqual(result, expected_result)
+
+    def test_execute(self):
+        epp = EppCommunicator(**self.epp_config)
+        epp.connect = MagicMock(return_value=None)
+        epp.greeting = "greeting"
+        raw_response = b'<?xml version="1.0" encoding="UTF-8"?>\n<epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:host="urn:ietf:params:xml:ns:host-1.0" xmlns:contact="urn:ietf:params:xml:ns:contact-1.0" xmlns:domain="urn:ietf:params:xml:ns:domain-1.0" xmlns:rgp="urn:ietf:params:xml:ns:rgp-1.0" xmlns:secDNS="urn:ietf:params:xml:ns:secDNS-1.1">\n    <response>\n        <result code="1000">\n            <msg>Command completed successfully</msg>\n        </result>\n        <trID>\n            <svTRID>CIRA-000057351729-0000000001</svTRID>\n        </trID>\n    </response>\n</epp>'
+        epp._execute_command = MagicMock(return_value=raw_response)
+        expected_result = \
+            {'code': 1000, 'message': 'Command completed successfully', 'reason': None, 'response': '''<response>
+<result code="1000">
+<msg>Command completed successfully</msg>
+</result>
+<trID>
+<svTRID>CIRA-000057351729-0000000001</svTRID>
+</trID>
+</response>'''}
+
+        epp.connect()
+        result = epp.execute(HELLO_XML)
+        self.assertDictEqual(result, expected_result)
+
+    def test_execute_epp_exception(self):
+        epp = EppCommunicator(**self.epp_config)
+        epp.connect = MagicMock(return_value=None)
+        epp.greeting = "greeting"
+        epp._execute_command = MagicMock(side_effect=EppCommunicatorException)
+
+        self.assertRaises(EppCommunicatorException, epp.execute, HELLO_XML)
+
+    def test_execute_general_exception(self):
+        epp = EppCommunicator(**self.epp_config)
+        epp.connect = MagicMock(return_value=None)
+        epp.greeting = "greeting"
+        epp._execute_command = MagicMock(side_effect=Exception)
+
+        self.assertRaises(EppCommunicatorException, epp.execute, HELLO_XML)
+
+    def test_read(self):
+        epp = EppCommunicator(**self.epp_config)
+        expected_result = b'<?xml version="1.0" encoding="UTF-8"?>\n<epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:fury="urn:ietf:params:xml:ns:fury-1.0" xmlns:droplist="urn:ietf:params:xml:ns:droplist-1.0" xmlns:idn="urn:ietf:params:xml:ns:idn-1.0" xmlns:host="urn:ietf:params:xml:ns:host-1.0" xmlns:contact="urn:ietf:params:xml:ns:contact-1.0" xmlns:domain="urn:ietf:params:xml:ns:domain-1.0" xmlns:secDNS="urn:ietf:params:xml:ns:secDNS-1.1" xmlns:launch="urn:ietf:params:xml:ns:launch-1.0" xmlns:mark="urn:ietf:params:xml:ns:mark-1.0" xmlns:smd="urn:ietf:params:xml:ns:signedMark-1.0" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:rgp="urn:ietf:params:xml:ns:rgp-1.0" xmlns:fee="urn:ietf:params:xml:ns:fee-0.11">\n    <greeting>\n        <svID>EPP Server Version: 8.0.4 (de4e6cbdab9536033563b335e4fa4555cf24c931)</svID>\n        <svDate>2023-03-30T20:56:22.740Z</svDate>\n        <svcMenu>\n            <version>1.0</version>\n            <lang>fr</lang>\n            <lang>en</lang>\n            <objURI>urn:ietf:params:xml:ns:epp-1.0</objURI>\n            <objURI>urn:ietf:params:xml:ns:domain-1.0</objURI>\n            <objURI>urn:ietf:params:xml:ns:host-1.0</objURI>\n            <objURI>urn:ietf:params:xml:ns:contact-1.0</objURI>\n            <svcExtension>\n                <extURI>urn:ietf:params:xml:ns:rgp-1.0</extURI>\n                <extURI>urn:ietf:params:xml:ns:fury-1.0</extURI>\n                <extURI>urn:ietf:params:xml:ns:fury-2.0</extURI>\n                <extURI>urn:ietf:params:xml:ns:droplist-1.0</extURI>\n                <extURI>urn:ietf:params:xml:ns:fury-rgp-1.0</extURI>\n                <extURI>urn:ietf:params:xml:ns:idn-1.0</extURI>\n                <extURI>urn:ietf:params:xml:ns:secDNS-1.1</extURI>\n                <extURI>urn:ietf:params:xml:ns:launch-1.0</extURI>\n                <extURI>urn:ietf:params:xml:ns:mark-1.0</extURI>\n                <extURI>urn:ietf:params:xml:ns:signedMark-1.0</extURI>\n                <extURI>urn:ietf:params:xml:ns:fee-0.11</extURI>\n                <extURI>urn:ietf:params:xml:ns:fee-0.9</extURI>\n                <extURI>http://www.w3.org/2000/09/xmldsig#</extURI>\n            </svcExtension>\n        </svcMenu>\n        <dcp>\n            <access>\n                <none/>\n            </access>\n            <statement>\n                <purpose>\n                    <admin/>\n                </purpose>\n                <recipient>\n                    <ours/>\n                </recipient>\n                <retention>\n                    <legal/>\n                </retention>\n            </statement>\n        </dcp>\n    </greeting>\n</epp>'
+        epp._ssl_socket = MagicMock(
+            read=MagicMock(return_value=b'\x00\x00\n\t'),
+            recv=MagicMock(return_value=expected_result)
+        )
+
+        result = epp._read()
+        self.assertEqual(result, expected_result)
+
+    def test_write(self):
+        epp = EppCommunicator(**self.epp_config)
+        expected_result = 103
+        epp._ssl_socket = MagicMock(
+            send=MagicMock(return_value=expected_result),
+        )
+
+        result = epp._write(HELLO_XML)
+
+        self.assertEqual(result, expected_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
