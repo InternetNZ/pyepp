153f27aa44244bd9a584ce23be9665bbf26fc367
Ehsan Faraji Jalal <ehsan@internetnz.net.nz>
2023-09-07 09:18:53 +1200

    Initial CLI setup (#43)
    * add click to requirements
    
    * initial cli setup
    
    * fix tests
    
    * lint
    
    * ignore cli in coverage for now
    
    * make epp config, user and pass arguments required
    
    * update the version

---
+++ pyepp/__main__.py
@@ -0,0 +1,40 @@
+"""
+Executable cli module.
+"""
+import logging
+
+import click
+
+from pyepp import cli
+
+logging.basicConfig(level=logging.CRITICAL)
+
+
+@click.group(name='pyepp')
+@click.option('--host', envvar="PYEPP_HOST", required=True)
+@click.option('--port', envvar="PYEPP_PORT", required=True)
+@click.option('--client-cert', envvar="PYEPP_CLIENT_CERT", required=True)
+@click.option('--client-key', envvar="PYEPP_CLIENT_KET", required=True)
+@click.option('--user', envvar="PYEPP_USER", required=True)
+@click.option('--password', envvar="PYEPP_PASSWORD", required=True)
+@click.option('-o', '--output-format', show_default=True, default='XML',
+              type=click.Choice(['XML', 'OBJECT'], case_sensitive=False))
+@click.option('--no-pretty', is_flag=True, show_default=True, default=False)
+@click.option('-v', '--verbose', is_flag=True, show_default=True, default=False)
+@click.option('-d', '--debug', is_flag=True, show_default=True, default=False)
+@click.pass_context
+# pylint: disable=too-many-arguments
+def pyepp_cli(ctx, host, port, client_cert, client_key, user, password, output_format, no_pretty, verbose, debug):
+    """A command line interface to work with PyEpp library."""
+    ctx.obj = cli.PyEppCli(host, port, client_cert, client_key, user, password, output_format, no_pretty)
+
+    if verbose:
+        logging.basicConfig(level=logging.INFO)
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+
+pyepp_cli.add_command(cli.contact)
+pyepp_cli.add_command(cli.domain)
+pyepp_cli.add_command(cli.host)
+pyepp_cli.add_command(cli.poll)
--- pyepp/helper.py
+++ pyepp/helper.py
@@ -4,6 +4,8 @@ Helper functions
 import random
 import string
 
+from bs4 import BeautifulSoup
+
 
 def generate_password(length: int) -> str:
     """Generate a random password including letters and digits.
@@ -14,3 +16,15 @@ def generate_password(length: int) -> str:
     :rtype: str
     """
     return ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(length)])  # nosec
+
+
+def xml_pretty(bxml: bytes) -> str:
+    """
+    Convert bytes xml to string and prettify it.
+
+    :param bxml: xml content
+
+    :return: xml in string
+    """
+    xml_str = BeautifulSoup(bxml, 'xml')
+    return xml_str.decode(pretty_print=True)
