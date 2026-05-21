f94ea7da15911bcc751146726791747d2582e9f5
Ehsan Faraji-Jalal <ehsan@internetnz.net.nz>
2026-05-22 10:54:43 +1200

    fix test_data_to_dict unit test

--- tests/test_poll.py
+++ tests/test_poll.py
@@ -1,6 +1,7 @@
 """
 Host Name Mapping unit tests
 """
+
 import unittest
 from datetime import date
 from unittest.mock import MagicMock
@@ -20,9 +21,15 @@ class HostTest(unittest.TestCase):
     def test_poll_request_unsuccessful(self) -> None:
         epp_communicator = MagicMock(EppCommunicator)
         poll = Poll(epp_communicator)
-        expected_result = \
-            EppResultData(**{'code': 2000, 'message': "Command completed unsuccessfully",
-                             'reason': None, 'raw_response': "response", 'result_data': None})
+        expected_result = EppResultData(
+            **{
+                "code": 2000,
+                "message": "Command completed unsuccessfully",
+                "reason": None,
+                "raw_response": "response",
+                "result_data": None,
+            }
+        )
         poll.execute = MagicMock(return_value=expected_result)
 
         result = poll.request()
@@ -32,36 +39,45 @@ class HostTest(unittest.TestCase):
     def test_poll_request(self) -> None:
         epp_communicator = MagicMock(EppCommunicator)
         poll = Poll(epp_communicator)
-        expected_result = \
-            EppResultData(**{'client_transaction_id': 'c6710aac-cbcf-48d0-9ec3-cfaad1cadc81',
-                             'code': 1301,
-                             'message': 'Command completed successfully; ack to dequeue',
-                             'raw_response': '<response>\n'
-                                             '<result code="1301">\n'
-                                             '<msg>Command completed successfully; ack to dequeue</msg>\n'
-                                             '</result>\n'
-                                             '<msgQ count="1" id="27690316">\n'
-                                             '<qDate>2023-07-26T00:21:31.246Z</qDate>\n'
-                                             '<msg lang="en">Contact inz-contact-1 has been '
-                                             'deleted.</msg>\n'
-                                             '</msgQ>\n'
-                                             '<trID>\n'
-                                             '<clTRID>c6710aac-cbcf-48d0-9ec3-cfaad1cadc81</clTRID>\n'
-                                             '<svTRID>CIRA-000097025501-0000000002</svTRID>\n'
-                                             '</trID>\n'
-                                             '</response>',
-                             'reason': None,
-                             'repository_object_id': None,
-                             'result_data': ServiceMessageQueueData(message_count='1',
-                                                                    message_id='27690316',
-                                                                    queue_date='2023-07-26T00:21:31.246Z',
-                                                                    messages=[ServiceMessageData(language='en',
-                                                                                                 message='Contact '
-                                                                                                         'inz-contact-1 '
-                                                                                                         'has '
-                                                                                                         'been '
-                                                                                                         'deleted.')]),
-                             'server_transaction_id': 'CIRA-000097025501-0000000002'})
+        expected_result = EppResultData(
+            **{
+                "client_transaction_id": "c6710aac-cbcf-48d0-9ec3-cfaad1cadc81",
+                "code": 1301,
+                "message": "Command completed successfully; ack to dequeue",
+                "raw_response": "<response>\n"
+                '<result code="1301">\n'
+                "<msg>Command completed successfully; ack to dequeue</msg>\n"
+                "</result>\n"
+                '<msgQ count="1" id="27690316">\n'
+                "<qDate>2023-07-26T00:21:31.246Z</qDate>\n"
+                '<msg lang="en">Contact inz-contact-1 has been '
+                "deleted.</msg>\n"
+                "</msgQ>\n"
+                "<trID>\n"
+                "<clTRID>c6710aac-cbcf-48d0-9ec3-cfaad1cadc81</clTRID>\n"
+                "<svTRID>CIRA-000097025501-0000000002</svTRID>\n"
+                "</trID>\n"
+                "</response>",
+                "reason": None,
+                "repository_object_id": None,
+                "result_data": ServiceMessageQueueData(
+                    message_count="1",
+                    message_id="27690316",
+                    queue_date="2023-07-26T00:21:31.246Z",
+                    messages=[
+                        ServiceMessageData(
+                            language="en",
+                            message="Contact "
+                            "inz-contact-1 "
+                            "has "
+                            "been "
+                            "deleted.",
+                        )
+                    ],
+                ),
+                "server_transaction_id": "CIRA-000097025501-0000000002",
+            }
+        )
 
         poll.execute = MagicMock(return_value=expected_result)
 
@@ -72,9 +88,15 @@ class HostTest(unittest.TestCase):
     def test_poll_ack_unsuccessful(self) -> None:
         epp_communicator = MagicMock(EppCommunicator)
         poll = Poll(epp_communicator)
-        expected_result = \
-            EppResultData(**{'code': 1500, 'message': "Command completed successfully",
-                             'reason': None, 'raw_response': "response", 'result_data': None})
+        expected_result = EppResultData(
+            **{
+                "code": 1500,
+                "message": "Command completed successfully",
+                "reason": None,
+                "raw_response": "response",
+                "result_data": None,
+            }
+        )
         poll.execute = MagicMock(return_value=expected_result)
 
         result = poll.acknowledge(121212)
@@ -84,26 +106,29 @@ class HostTest(unittest.TestCase):
     def test_poll_ack(self) -> None:
         epp_communicator = MagicMock(EppCommunicator)
         poll = Poll(epp_communicator)
-        
-        expected_result = \
-            EppResultData(**{'client_transaction_id': 'c6710aac-cbcf-48d0-9ec3-cfaad1cadc81',
-                             'code': 1000,
-                             'message': 'Command completed successfully',
-                             'raw_response': '<response>\n'
-                                             '<result code="1000">\n'
-                                             '<msg>Command completed successfully</msg>\n'
-                                             '</result>\n'
-                                             '<msgQ count="1" id="27690316">\n'
-                                             '</msgQ>\n'
-                                             '<trID>\n'
-                                             '<clTRID>c6710aac-cbcf-48d0-9ec3-cfaad1cadc81</clTRID>\n'
-                                             '<svTRID>CIRA-000097025501-0000000002</svTRID>\n'
-                                             '</trID>\n'
-                                             '</response>',
-                             'reason': None,
-                             'repository_object_id': None,
-                             'result_data': None,
-                             'server_transaction_id': 'CIRA-000097025501-0000000002'})
+
+        expected_result = EppResultData(
+            **{
+                "client_transaction_id": "c6710aac-cbcf-48d0-9ec3-cfaad1cadc81",
+                "code": 1000,
+                "message": "Command completed successfully",
+                "raw_response": "<response>\n"
+                '<result code="1000">\n'
+                "<msg>Command completed successfully</msg>\n"
+                "</result>\n"
+                '<msgQ count="1" id="27690316">\n'
+                "</msgQ>\n"
+                "<trID>\n"
+                "<clTRID>c6710aac-cbcf-48d0-9ec3-cfaad1cadc81</clTRID>\n"
+                "<svTRID>CIRA-000097025501-0000000002</svTRID>\n"
+                "</trID>\n"
+                "</response>",
+                "reason": None,
+                "repository_object_id": None,
+                "result_data": None,
+                "server_transaction_id": "CIRA-000097025501-0000000002",
+            }
+        )
 
         poll.execute = MagicMock(return_value=expected_result)
 
@@ -116,6 +141,16 @@ class HostTest(unittest.TestCase):
     def test_data_to_dict(self) -> None:
         epp_communicator = MagicMock(EppCommunicator)
         poll = Poll(epp_communicator)
-        data = ServiceMessageData(language='en', message='Test msg')
+        data = ServiceMessageQueueData(
+            message_count=1,
+            message_id=123,
+            queue_date='2023-01-01T00:00:00.000Z',
+            messages=[ServiceMessageData(language='en', message='Test msg')]
+        )
         result = poll._data_to_dict(data)
-        self.assertDictEqual(result, {'language': 'en', 'message': 'Test msg'})
+        self.assertDictEqual(result, {
+            'message_count': 1,
+            'message_id': 123,
+            'queue_date': '2023-01-01T00:00:00.000Z',
+            'messages': [{'language': 'en', 'message': 'Test msg'}]
+        })
